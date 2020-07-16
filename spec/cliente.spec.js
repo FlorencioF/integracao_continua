7d4276836fbd86c6bdd053162f29bf8ac4c4f824
FlorencioF <fur_oliveira@hotmail.com>
2020-07-16 18:03:21 -0300

    Automação de testes

---
+++ spec/cliente.spec.js
@@ -0,0 +1,20 @@
+const Cliente = require('../models/cliente');
+const { ExpectationFailed } = require('http-errors');
+
+test('Testando se o modelo cliente tem a propriedade nome', () => {
+    let cliente = new Cliente();
+    cliente.nome = 'Fernando';
+    expect(cliente.nome).not.toBeUndefined();
+});
+
+test('Testando se o modelo cliente tem a propriedade cpf', () => {
+    let cliente = new Cliente();
+    cliente.cpf = '444.222.333-66';
+    expect(cliente.cpf).not.toBeUndefined();
+});
+
+test('Validando o cpf', () => {
+    let cliente = new Cliente();
+    cliente.cpf = '423.467.658-64';
+    expect(cliente.validarCPF()).toBe(true);
+});
